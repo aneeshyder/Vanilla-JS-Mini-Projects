c9a7b5f19e94673f0d480ae899c1e73ed5afb45c
aneeshyder <anishcharoliya@gmail.com>
2021-10-30 17:46:54 +0530

    Form Validator added

---
+++ Form Validator/script.js
@@ -0,0 +1,98 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+let errors = {
+    username: false,
+    email: false,
+    password: false,
+    password2: false,
+}
+
+function showError(input, message){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+// Check email
+function checkEmail(input) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if (re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, 'Email is not valid');
+    }
+}
+
+
+// Check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    } else if (input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
+}
+
+// Check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+         showError(input2, 'Passwords do not match');
+         console.log('pass mactch done');
+    }
+}
+
+// Get fieldname
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+
+// Check required fields
+function checkRequired(inputArr){
+    inputArr.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input, `${getFieldName(input)} is Required`);
+            errors[input.id] = false;
+        } else {
+            showSuccess(input);
+            errors[input.id] = true;
+        }
+    });
+}
+
+
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+
+    checkRequired([username, email, password, password2]);
+
+    if (errors.username) {
+        checkLength(username, 3, 15);
+    }
+
+    if (errors.password) {
+        checkLength(password, 6, 25);
+    }
+
+    if (errors.email) {
+        checkEmail(email);
+    }
+
+    if (errors.password && password2) {
+        checkPasswordsMatch(password, password2);
+    }
+    
+});
